44b68eb98594545e9dc27d466c667603e9989da8
Owen Trutwein <owen.trutwein@outlook.com>
2023-08-07 20:45:24 +0100

    Initial commit - pushing "Dog" class.

---
+++ src/Dog.java
@@ -0,0 +1,20 @@
+public class Dog {
+    public String name;
+    public String breed;
+    public int ageInYears;
+    public char gender;
+    public double weightInPounds;
+    public boolean vaccinated;
+    String newline = System.getProperty("line.separator");
+
+    public void display() {
+        System.out.println(newline);
+        System.out.println("Name: " + name);
+        System.out.println("Breed: " + breed);
+        System.out.println("Gender: " + gender);
+        System.out.println("Age in years: " + ageInYears);
+        System.out.println("Weight in lb: " + weightInPounds);
+        System.out.println("Chubby: " + vaccinated);
+        System.out.println(newline);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Dog dog1 = new Dog();
+        dog1.name = "Hooch";
+        dog1.breed = "Boxer";
+        dog1.ageInYears = 2;
+        dog1.gender = 'M';
+        dog1.weightInPounds = 10.5;
+        dog1.vaccinated = false;
+        dog1.display();
+
+        Dog dog2 = new Dog();
+        dog2.name = "Billy";
+        dog2.breed = "British Bulldog";
+        dog2.ageInYears = 5;
+        dog2.gender = 'F';
+        dog2.weightInPounds = 5.5;
+        dog2.vaccinated = true;
+        dog2.display();
+
+        Dog dog3 = new Dog();
+        dog3.name = "Jack";
+        dog3.breed = "Staffordshire Bull Terrier";
+        dog3.gender = 'M';
+        dog3.ageInYears = 3;
+        dog3.weightInPounds = 5.0;
+        dog3.vaccinated = true;
+        dog3.display();
+    }
+}
